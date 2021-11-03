30240c114a854d54f370feef3812d99e536bc429
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2021-11-03 17:49:57 -0400

    added jwt and auth validation.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("./models/User");
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
-
+const requireAuth = require("./middlewares/requireAuth");
 const app = express();
 
 app.use(express.json());
@@ -21,8 +21,8 @@ mongoose.connection.on("error", (err) => {
   console.error("Error connecting to mongo instance", err);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hi there!");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {

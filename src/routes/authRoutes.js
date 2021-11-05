0074e652223adfe1a13304fe1369d45e25ff4a72
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2021-11-05 15:25:23 -0400

    added hash and salt to user password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -19,4 +19,25 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/singin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(422).send({ error: "Email not found" });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "key");
+    res.send({ key });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid pass" });
+  }
+});
+
 module.exports = router;
